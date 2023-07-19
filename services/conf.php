cb4f0f5712067fd65a02d86e8cb12e18b01d0f50
morondi <morondibrian67@gmail.com>
2023-07-19 15:09:18 +0300

    config

--- services/conf.php
+++ services/conf.php
@@ -6,10 +6,10 @@ class CONF {
     public $DEMO_VERSION = false;
 
     /* Data configuration for database */
-    public $DB_SERVER   = "localhost";
-    public $DB_USER     = "appsians_amin";
-    public $DB_PASSWORD = "appsians_amin";
-    public $DB_NAME     = "appsians_testapi";
+    public $DB_SERVER   = "blaxwohrknb10tbsqojn-mysql.services.clever-cloud.com";
+    public $DB_USER     = "ulpluwz2knunxyb4";
+    public $DB_PASSWORD = "6Tt4vWoVy9KCiHukG38U";
+    public $DB_NAME     = "blaxwohrknb10tbsqojn";
 
     /* [ IMPORTANT ] be careful when edit this security code, use AlphaNumeric only*/
     /* This string must be same with security code at Android, if its different android unable to submit order */
